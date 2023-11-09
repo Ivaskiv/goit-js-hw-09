9a71ec2a0f1f540abf3e25861c90d944b967f39d
Ivaskiv <viraivaskiv@gmail.com>
2023-11-09 21:32:57 +0000

    Deploying to gh-pages from @ Ivaskiv/goit-js-hw-09@7b415a3f9da98076252219f4b176b502110cf2d7 🚀

--- 03-promises.4c0746a7.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form");const e=document.querySelector('input[name="delay"]'),o=document.querySelector('input[name="step"]');document.querySelector('input[name="amount"]');console.log(e),console.log(o);
-//# sourceMappingURL=03-promises.4c0746a7.js.map
